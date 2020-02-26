5b9b09cbfc0541d8f4373c6f1dc9dcaa15eec0d5
pandersen13 <55473525+pandersen13@users.noreply.github.com>
2020-02-26 17:28:45 +1100

    Modify delta calculator to add acute diseases

--- src/vivarium_unimelb_tobacco_intervention_comparison/internal_data/disease_delta.py
+++ src/vivarium_unimelb_tobacco_intervention_comparison/internal_data/disease_delta.py
@@ -5,16 +5,15 @@ import os
 import numpy as np
 import pandas as pd
 
-import pdb, traceback, sys
-
 from vivarium.framework.artifact.artifact import Artifact
 
 
-class chronicDiseaseDeltaCalculator:
+class diseaseDeltaCalculator:
 
-    def __init__(self, model, artifact_path, disease_list):
+    def __init__(self, model, artifact_path, chronic_disease_list, acute_disease_list):
         self.model = model
-        self.disease_list = disease_list
+        self.chronic_disease_list = chronic_disease_list
+        self.acute_disease_list = acute_disease_list
         
         self.key_columns = ['age_start', 'age_end', 'sex',
                             'year_start', 'year_end']
@@ -25,13 +24,16 @@ class chronicDiseaseDeltaCalculator:
         self.disease_disability_data = {}
         self.bau_mortality_tables = {}
         self.bau_prevalence_tables = {}
+        self.acute_disease_excess_mort = {}
+        self.acute_disease_yld = {}
+
         self.mortality_deltas = pd.DataFrame()
         self.yld_deltas = pd.DataFrame()
 
         artifact = Artifact(artifact_path)
 
-        for disease in disease_list:
-            self.disease_disability_data[disease] = self.load_disability_data(artifact, disease)
+        for disease in chronic_disease_list:
+            self.disease_disability_data[disease] = self.load_chronic_disability_data(artifact, disease)
             self.bau_mortality_tables[disease] = (self.load_simulated_disease_data(self.model, 
                                                                                    self.bau_scenario, 
                                                                                    disease,
@@ -43,13 +45,41 @@ class chronicDiseaseDeltaCalculator:
                                                                                     disease,
                                                                                     'prevalence')
                                                  .set_index(['age','sex','year'])
-            )          
+            )
+
+        for disease in acute_disease_list:
+            self.acute_disease_excess_mort[disease] = self.load_acute_mortality_data(artifact, disease)
+            self.acute_disease_yld[disease] = self.load_acute_disability_data(artifact, disease)
 
 
-    def compute_mortality_deltas(self, start_year, end_year, scenario):
+    def compute_deltas(self, start_year, end_year, scenario):
+        acute_pifs = {}
+        for disease in self.acute_disease_list:
+            acute_pifs[disease] = self.load_pif(disease, scenario)
+
+        self.mortality_deltas = 0
+        self.mortality_deltas += self.compute_chronic_mortality_deltas(start_year,
+                                                                       end_year,
+                                                                       scenario)
+        self.mortality_deltas += self.compute_acute_mortality_deltas(start_year,
+                                                                     end_year,
+                                                                     scenario,
+                                                                     acute_pifs)
+
+        self.yld_deltas = 0                                                            
+        self.yld_deltas += self.compute_chronic_yld_deltas(start_year,
+                                                           end_year,
+                                                           scenario)
+        self.yld_deltas += self.compute_acute_yld_deltas(start_year,
+                                                         end_year,
+                                                         scenario,
+                                                         acute_pifs)            
+
+
+    def compute_chronic_mortality_deltas(self, start_year, end_year, scenario):
         total_delta = 0
 
-        for disease in self.disease_list:
+        for disease in self.chronic_disease_list:
             mortality_risk_int = (self.load_simulated_disease_data(self.model, 
                                                                           scenario, 
                                                                           disease,
@@ -60,13 +90,32 @@ class chronicDiseaseDeltaCalculator:
             delta = np.log((1 - mortality_risk) / (1 - mortality_risk_int))
             total_delta += delta
         
-        self.mortality_deltas = total_delta
+        return total_delta
 
-
-    def compute_yld_deltas(self, start_year, end_year, scenario):
+    
+    def compute_acute_mortality_deltas(self, start_year, end_year, scenario, acute_pifs):
         total_delta = 0
 
-        for disease in self.disease_list:
+        for disease in self.acute_disease_list:
+            excess_mortality_data = self.acute_disease_excess_mort[disease]
+            pif = acute_pifs[disease]
+            disease_delta = pd.DataFrame()
+            
+            for year in range(start_year, end_year+1):
+                pif_subtable = pif.xs(year, level = 'year', drop_level=False)
+                mort_subtable = excess_mortality_data.loc[pif_subtable.index]
+                delta = -mort_subtable*pif_subtable
+                disease_delta = disease_delta.append(delta)
+
+            total_delta += disease_delta
+
+        return total_delta
+
+
+    def compute_chronic_yld_deltas(self, start_year, end_year, scenario):
+        total_delta = 0
+
+        for disease in self.chronic_disease_list:
             disability_table = self.disease_disability_data[disease]
             disease_delta = pd.DataFrame()
             prevalence_rate_int = (self.load_simulated_disease_data(self.model, 
@@ -92,7 +141,26 @@ class chronicDiseaseDeltaCalculator:
             
             total_delta += disease_delta
 
-        self.yld_deltas = total_delta
+        return total_delta
+
+
+    def compute_acute_yld_deltas(self, start_year, end_year, scenario, acute_pifs):
+        total_delta = 0
+
+        for disease in self.acute_disease_list:
+            yld_data = self.acute_disease_yld[disease]
+            pif = acute_pifs[disease]
+            disease_delta = pd.DataFrame()
+            
+            for year in range(start_year, end_year+1):
+                pif_subtable = pif.xs(year, level = 'year', drop_level=False)
+                yld_subtable = yld_data.loc[pif_subtable.index]
+                delta = -yld_subtable*pif_subtable
+                disease_delta = disease_delta.append(delta)
+
+            total_delta += disease_delta
+
+        return total_delta
 
                                 
     def load_simulated_disease_data(self, model, scenario, disease, rate_name):
@@ -105,7 +173,7 @@ class chronicDiseaseDeltaCalculator:
         return pd.read_csv(self.disease_results_prefix + disease_rate_filename)
     
 
-    def load_disability_data(self, artifact, disease):
+    def load_chronic_disability_data(self, artifact, disease):
         key = f'chronic_disease.{disease}.morbidity'
         disability_data = pivot_load(artifact, key)
 
@@ -124,6 +192,54 @@ class chronicDiseaseDeltaCalculator:
 
         return disability_data
 
+
+    def load_acute_disability_data(self, artifact, disease):
+        key = f'acute_disease.{disease}.morbidity'
+        disability_data = pivot_load(artifact, key)
+
+        # Check that the morbidity table includes required columns.
+        if ( 
+            set(self.key_columns) 
+            & set(disability_data.columns) 
+            != set(self.key_columns)
+        ):
+            msg = 'Missing index columns for morbidity rates'
+            raise ValueError(msg) 
+
+        disability_data.rename(columns={'age_start': 'age'}, inplace=True)
+        disability_data.drop(['year_start','year_end','age_end', 'draw'], axis=1, inplace=True)
+        disability_data.set_index(['age','sex'], inplace=True)
+
+        return disability_data
+
+
+    def load_acute_mortality_data(self, artifact, disease):
+        key = f'acute_disease.{disease}.mortality'
+        mortality_data = pivot_load(artifact, key)
+
+        # Check that the mortality table includes required columns.
+        if ( 
+            set(self.key_columns) 
+            & set(mortality_data.columns) 
+            != set(self.key_columns)
+        ):
+            msg = 'Missing index columns for mortality rates'
+            raise ValueError(msg) 
+
+        mortality_data.rename(columns={'age_start': 'age'}, inplace=True)
+        mortality_data.drop(['year_start','year_end','age_end', 'draw'], axis=1, inplace=True)
+        mortality_data.set_index(['age','sex'], inplace=True)
+
+        return mortality_data
+
+
+    def load_pif(self, disease, scenario):
+        pif_folder = 'pif_results/{}/{}/'.format(self.model, disease)
+        pif_filename = '{}_pifs_{}_{}.csv'.format(self.model, disease, scenario)
+
+        pif_data = pd.read_csv(pif_folder + pif_filename).set_index(['age', 'sex', 'year'])
+        
+        return pif_data
 
     def write_delta_table(self, output_prefix, scenario):
         directory = './{}{}'.format(output_prefix, self.model)
@@ -186,17 +302,17 @@ def pivot_load(artifact, entity_key):
 
 model = 'test_exposure'
 artifact_path = 'C:\\Users\\andersenp\\Documents\\Python_CEB_cloudless\\vivarium_tobacco\\vivarium_unimelb_tobacco_intervention_comparison/artifacts/mslt_tobacco_maori_20-years.hdf'
-disease_list = ['CHD', 'Stroke']
+chronic_disease_list = ['CHD', 'Stroke']
+acute_disease_list = ['LRTI']
 #disease_list = ['CHD']
 scenario = 'tax'
 output_prefix = 'disease_delta_results/'
 
-chronic_delta_calc = chronicDiseaseDeltaCalculator(model, artifact_path, disease_list) 
-chronic_delta_calc.compute_mortality_deltas(2011, 2020, scenario)
-chronic_delta_calc.compute_yld_deltas(2011, 2020, scenario)
-chronic_delta_calc.write_delta_series(output_prefix, scenario, 'mortality')
-chronic_delta_calc.write_delta_series(output_prefix, scenario, 'yld')
-#chronic_delta_calc.write_delta_table(output_prefix, scenario)
+delta_calc = diseaseDeltaCalculator(model, artifact_path, chronic_disease_list, acute_disease_list)
+delta_calc.compute_deltas(2011, 2020, scenario)
+delta_calc.write_delta_series(output_prefix, scenario, 'mortality')
+delta_calc.write_delta_series(output_prefix, scenario, 'yld')
+#delta_calc.write_delta_table(output_prefix, scenario)
 
 
 
